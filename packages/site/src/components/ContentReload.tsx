3449f0337142493f6e972da8a4342447369b6d3b
Rowan Cockett <rowanc1@gmail.com>
2022-12-15 11:06:17 -0700

    📜 Improved content reload with scroll (#15)
    * 📜 Improved content reload with scroll
    * 📦 Bring dev check inside

--- packages/site/src/components/ContentReload.tsx
+++ packages/site/src/components/ContentReload.tsx
@@ -1,52 +1,66 @@
+import { useEffect } from 'react';
+
+const STORAGE_KEY = 'myst';
+
+async function mystLiveReloadConnect(config: { onOpen?: () => void; port?: string | number }) {
+  if (!config.port || (window as any).mystLiveReloadConnected) return;
+  (window as any).mystLiveReloadConnected = true;
+  setTimeout(() => {
+    const myst = JSON.parse(sessionStorage.getItem(STORAGE_KEY) || '{}');
+    if (myst.scroll) {
+      window.scrollTo(0, myst.scroll);
+      sessionStorage.removeItem(STORAGE_KEY);
+    }
+  }, 30);
+  console.log(`🔊 Listening to live content changes on port ${config.port}`);
+  const protocol = location.protocol === 'https:' ? 'wss:' : 'ws:';
+  const host = location.hostname;
+  const socketPath = `${protocol}//${host}:${config.port}/socket`;
+  const ws = new WebSocket(socketPath);
+
+  ws.onmessage = (message) => {
+    const event = JSON.parse(message.data);
+    if (event.type === 'LOG') {
+      console.log(event.message);
+    }
+    if (event.type === 'RELOAD') {
+      console.log('🚀 Reloading window ...');
+      console.log(`📌 Keeping scroll for page at ${window.scrollY}`);
+      const myst = JSON.parse(sessionStorage.getItem(STORAGE_KEY) || '{}');
+      myst.scroll = window.scrollY;
+      sessionStorage.setItem(STORAGE_KEY, JSON.stringify(myst));
+      window.location.reload();
+    }
+  };
+  ws.onopen = () => {
+    if (config && typeof config.onOpen === 'function') {
+      config.onOpen();
+    }
+  };
+  ws.onclose = () => {
+    console.log('MyST content server web socket closed. Reconnecting...');
+    setTimeout(
+      () =>
+        mystLiveReloadConnect({
+          ...config,
+          onOpen: () => window.location.reload(),
+        }),
+      1000,
+    );
+  };
+  ws.onerror = (error: any) => {
+    console.log('MyST content server web socket error:');
+    console.error(error);
+  };
+}
+
 // Inspired by the LiveReload component in Remix
 export const ContentReload =
   process.env.NODE_ENV !== 'development'
     ? () => null
     : ({ port }: { port?: string | number }) => {
-        if (!port) return null;
-        return (
-          <script
-            suppressHydrationWarning
-            dangerouslySetInnerHTML={{
-              __html: `
-                function mystLiveReloadConnect(config) {
-                  let protocol = location.protocol === "https:" ? "wss:" : "ws:";
-                  let host = location.hostname;
-                  let socketPath = protocol + "//" + host + ":" + ${port} + "/socket";
-                  let ws = new WebSocket(socketPath);
-                  ws.onmessage = (message) => {
-                    let event = JSON.parse(message.data);
-                    if (event.type === "LOG") {
-                      console.log(event.message);
-                    }
-                    if (event.type === "RELOAD") {
-                      console.log("🚀 Reloading window ...");
-                      window.location.reload();
-                    }
-                  };
-                  ws.onopen = () => {
-                    if (config && typeof config.onOpen === "function") {
-                      config.onOpen();
-                    }
-                  };
-                  ws.onclose = (error) => {
-                    console.log("MyST content server web socket closed. Reconnecting...");
-                    setTimeout(
-                      () =>
-                        mystLiveReloadConnect({
-                          onOpen: () => window.location.reload(),
-                        }),
-                      1000
-                    );
-                  };
-                  ws.onerror = (error) => {
-                    console.log("MyST content server web socket error:");
-                    console.error(error);
-                  };
-                }
-                mystLiveReloadConnect();
-              `,
-            }}
-          />
-        );
+        useEffect(() => {
+          mystLiveReloadConnect({ port });
+        }, []);
+        return null;
       };

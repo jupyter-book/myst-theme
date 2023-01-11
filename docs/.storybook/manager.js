39d8aeb905dceaef78fd2eec2581952ef3318f5e
Rowan Cockett <rowanc1@gmail.com>
2023-01-11 15:35:27 -0700

    📖 Docs using storybook

---
+++ docs/.storybook/manager.js
@@ -0,0 +1,8 @@
+// .storybook/manager.js
+
+import { addons } from '@storybook/addons';
+import theme from './myst-theme';
+
+addons.setConfig({
+  theme,
+});
---
+++ docs/.storybook/myst-theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: '@myst-theme',
+  brandUrl: '/',
+  brandImage: '/logo.svg',
+  brandTarget: '_self',
+});

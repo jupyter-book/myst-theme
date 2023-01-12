700a990ebcbd9e6e998fbb7cd78c0350160a7598
Rowan Cockett <rowanc1@gmail.com>
2023-01-12 15:51:47 -0700

    🔗 Fix docs branding link

--- docs/.storybook/myst-theme.js
+++ docs/.storybook/myst-theme.js
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming';
 export default create({
   base: 'light',
   brandTitle: '@myst-theme',
-  brandUrl: '/',
+  brandUrl: './', // On github pages, this is a non-bare URL
   brandImage: 'logo.svg',
   brandTarget: '_self',
 });

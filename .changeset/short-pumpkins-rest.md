1db718806177860d5b60e91daa196bac2a632ce5
Rowan Cockett <rowanc1@gmail.com>
2023-02-02 15:43:37 -0700

    🐛 Fix up the navigation in face of temporary bug (#27)

---
+++ .changeset/short-pumpkins-rest.md
@@ -0,0 +1,5 @@
+---
+'@myst-theme/site': patch
+---
+
+Strange bug in active theme, add spaces around the created class as a temporary fix to allow the classes to move through
--- packages/site/src/components/Navigation/TopNav.tsx
+++ packages/site/src/components/Navigation/TopNav.tsx
@@ -109,7 +109,7 @@ function NavItem({ item }: { item: SiteNavItem }) {
                   to={action.url || ''}
                   className={({ isActive }) =>
                     classNames(
-                      'block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-black',
+                      ' block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-black ',
                       {
                         'text-black font-bold': isActive,
                       },

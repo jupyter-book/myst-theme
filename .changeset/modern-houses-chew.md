a1bef25e52dbcf0eb1388bcc22adea37c41c66d5
Rowan Cockett <rowanc1@gmail.com>
2023-02-02 20:24:44 -0700

    🐛 Update URL for MadeWithMyST

---
+++ .changeset/modern-houses-chew.md
@@ -0,0 +1,5 @@
+---
+'@myst-theme/icons': patch
+---
+
+Change the target url for madewithmyst
--- packages/icons/src/myst.tsx
+++ packages/icons/src/myst.tsx
@@ -34,11 +34,11 @@ export function MystLogo({
   );
 }
 
-export function MadeWithMyst() {
+export function MadeWithMyst({ url = 'https://myst-tools.org/made-with-myst' }: { url?: string }) {
   return (
     <a
       className="flex w-fit mx-auto text-gray-700 hover:text-blue-700 dark:text-gray-200 dark:hover:text-blue-400"
-      href="https://curvenote.com"
+      href={url}
       target="_blank"
       rel="noreferrer"
     >

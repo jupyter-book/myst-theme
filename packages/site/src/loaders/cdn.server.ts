3c12c671439f18a2173c7c2c276e1aca2a7e3743
Rowan Cockett <rowanc1@gmail.com>
2023-03-10 17:45:48 -0700

    📦 Update the CDN to allow for different hosts

--- packages/site/src/loaders/cdn.server.ts
+++ packages/site/src/loaders/cdn.server.ts
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 import NodeCache from 'node-cache';
-import type { SiteManifest as Config } from 'myst-config';
+import type { SiteManifest } from 'myst-config';
 import { responseNoArticle, responseNoSite } from './errors.server';
 import {
   getFooterLinks,
@@ -21,7 +21,9 @@ declare global {
   var cdnRouterCache: NodeCache | undefined, configCache: NodeCache | undefined;
 }
 
-const CDN = 'https://cdn.curvenote.com/';
+export type Host = string | { CDN: string; id: string };
+
+const DEFAULT_CDN = 'https://cdn.curvenote.com/';
 
 function getCdnRouterCache() {
   if (global.cdnRouterCache) return global.cdnRouterCache;
@@ -31,7 +33,7 @@ function getCdnRouterCache() {
   return global.cdnRouterCache;
 }
 
-function getConfigCache() {
+export function getConfigCache() {
   if (global.configCache) return global.configCache;
   console.log('Creating configCache');
   // The config can be long lived as it is static (0 == ∞)
@@ -52,9 +54,27 @@ async function getCdnPath(hostname: string): Promise<string | undefined> {
   return data.cdn;
 }
 
-function withCDN<T extends string | undefined>(id: string, url: T): T {
+function withPublicFolderUrl(baseUrl: string, url: string): string {
+  return withBaseUrl(baseUrl, `public${url}`);
+}
+
+export async function getCdnLocation(host: Host) {
+  if (typeof host === 'string') {
+    const id = await getCdnPath(host);
+    if (!id) throw responseNoSite();
+    return { CDN: DEFAULT_CDN, id };
+  }
+  return host;
+}
+
+export async function getCdnBaseUrl(host: Host): Promise<string> {
+  const { CDN, id } = await getCdnLocation(host);
+  return `${CDN}${id}/`;
+}
+
+function withBaseUrl<T extends string | undefined>(baseUrl: string, url: T): T {
   if (!url) return url;
-  return `${CDN}${id}/public${url}` as T;
+  return `${baseUrl}${url}` as T;
 }
 
 /**
@@ -67,7 +87,7 @@ function foldTitleString(title?: string): string | undefined {
 /**
  * If the site title and the first nav item are the same, remove it.
  */
-function removeSingleNavItems(config: Config) {
+function removeSingleNavItems(config: SiteManifest) {
   if (
     config?.nav?.length === 1 &&
     foldTitleString(config.nav[0].title) === foldTitleString(config.title)
@@ -76,48 +96,51 @@ function removeSingleNavItems(config: Config) {
   }
 }
 
-export async function getConfig(hostname: string): Promise<Config> {
-  const id = await getCdnPath(hostname);
+export async function getConfig(host: Host): Promise<SiteManifest> {
+  const location = await getCdnLocation(host);
+  const baseUrl = await getCdnBaseUrl(location);
+  const { id } = location;
   if (!id) throw responseNoSite();
-  const cached = getConfigCache().get<Config>(id);
+  const cached = getConfigCache().get<SiteManifest>(id);
   // Load the data from an in memory cache.
   if (cached) return cached;
-  const response = await fetch(`${CDN}${id}/config.json`);
+  const response = await fetch(withBaseUrl(baseUrl, 'config.json'));
   if (response.status === 404) throw responseNoSite();
-  const data = (await response.json()) as Config;
+  const data = (await response.json()) as SiteManifest;
   data.id = id;
   removeSingleNavItems(data);
-  updateSiteManifestStaticLinksInplace(data, (url) => withCDN(id, url));
-  getConfigCache().set<Config>(id, data);
+  updateSiteManifestStaticLinksInplace(data, (url) => withPublicFolderUrl(baseUrl, url));
+  getConfigCache().set<SiteManifest>(id, data);
   return data;
 }
 
-export async function getObjectsInv(hostname: string): Promise<Buffer | undefined> {
-  const id = await getCdnPath(hostname);
-  if (!id) return;
-  const url = `${CDN}${id}/objects.inv`;
+export async function getObjectsInv(host: Host): Promise<Buffer | undefined> {
+  const baseUrl = await getCdnBaseUrl(host);
+  if (!baseUrl) return;
+  const url = `${baseUrl}objects.inv`;
   const response = await fetch(url);
   if (response.status === 404) return;
   const buffer = await response.buffer();
   return buffer;
 }
 
-export async function getData(
-  config?: Config,
+async function getData(
+  baseUrl: string,
+  config?: SiteManifest,
   project?: string,
   slug?: string,
 ): Promise<PageLoader | null> {
   if (!project || !slug || !config) throw responseNoArticle();
   const { id } = config;
   if (!id) throw responseNoSite();
-  const response = await fetch(`${CDN}${id}/content/${project}/${slug}.json`);
+  const response = await fetch(withBaseUrl(baseUrl, `content/${project}/${slug}.json`));
   if (response.status === 404) throw responseNoArticle();
   const data = (await response.json()) as PageLoader;
-  return updatePageStaticLinksInplace(data, (url) => withCDN(id, url));
+  return updatePageStaticLinksInplace(data, (url) => withPublicFolderUrl(baseUrl, url));
 }
 
 export async function getPage(
-  hostname: string,
+  host: Host,
   opts: {
     domain?: string;
     project?: string;
@@ -127,7 +150,8 @@ export async function getPage(
   },
 ): Promise<PageLoader | Response | null> {
   const projectName = opts.project;
-  const config = await getConfig(hostname);
+  const baseUrl = await getCdnBaseUrl(host);
+  const config = await getConfig(host);
   if (!config) throw responseNoSite();
   const project = getProject(config, projectName);
   if (!project) throw responseNoArticle();
@@ -135,7 +159,7 @@ export async function getPage(
     return redirect(`${typeof opts.redirect === 'string' ? opts.redirect : '/'}${projectName}`);
   }
   const slug = opts.loadIndexPage || opts.slug == null ? project.index : opts.slug;
-  const loader = await getData(config, projectName, slug).catch((e) => {
+  const loader = await getData(baseUrl, config, projectName, slug).catch((e) => {
     console.error(e);
     return null;
   });

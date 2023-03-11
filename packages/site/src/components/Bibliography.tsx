c0989837de7d0ab3344915826eb873a04ac80327
Rowan Cockett <rowanc1@gmail.com>
2023-03-10 17:46:20 -0700

    🐛 Show all references on one line

--- packages/site/src/components/Bibliography.tsx
+++ packages/site/src/components/Bibliography.tsx
@@ -12,15 +12,17 @@ export function Bibliography() {
   const refs = hidden ? filtered.slice(0, HIDE_OVER_N_REFERENCES) : filtered;
   return (
     <section className="article-grid article-subgrid-gap col-screen">
-      {filtered.length > HIDE_OVER_N_REFERENCES && (
-        <button
-          onClick={() => setHidden(!hidden)}
-          className="float-right text-xs p-1 px-2 border rounded hover:border-blue-500 dark:hover:border-blue-400"
-        >
-          {hidden ? 'Show All' : 'Collapse'}
-        </button>
-      )}
-      <header className="text-lg font-semibold text-stone-900 dark:text-white">References</header>
+      <div>
+        {filtered.length > HIDE_OVER_N_REFERENCES && (
+          <button
+            onClick={() => setHidden(!hidden)}
+            className="float-right text-xs p-1 px-2 border rounded hover:border-blue-500 dark:hover:border-blue-400"
+          >
+            {hidden ? 'Show All' : 'Collapse'}
+          </button>
+        )}
+        <header className="text-lg font-semibold text-stone-900 dark:text-white">References</header>
+      </div>
       <div className="text-xs mb-8 pl-3 text-stone-500 dark:text-stone-300">
         <ol>
           {refs.map((label) => {

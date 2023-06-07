6b93711002a11eea37c85d252a2adfcd702e4316
Rowan Cockett <rowanc1@gmail.com>
2023-06-07 09:10:55 -0700

    🐾 No shrink on arrows in footer block (#115)

--- packages/site/src/components/FooterLinksBlock.tsx
+++ packages/site/src/components/FooterLinksBlock.tsx
@@ -21,14 +21,14 @@ const FooterLink = ({
     >
       <div className="flex align-middle h-full">
         {right && (
-          <ArrowLeftIcon className="w-6 h-6 self-center transition-transform group-hover:-translate-x-1" />
+          <ArrowLeftIcon className="w-6 h-6 self-center transition-transform group-hover:-translate-x-1 shrink-0" />
         )}
         <div className={classNames('flex-grow', { 'text-right': right })}>
           <div className="text-xs text-gray-500 dark:text-gray-400">{group || ' '}</div>
           {short_title || title}
         </div>
         {!right && (
-          <ArrowRightIcon className="w-6 h-6 self-center transition-transform group-hover:translate-x-1" />
+          <ArrowRightIcon className="w-6 h-6 self-center transition-transform group-hover:translate-x-1 shrink-0" />
         )}
       </div>
     </Link>

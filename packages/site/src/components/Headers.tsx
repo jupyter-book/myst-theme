2158b5740248ead0ab3631607ebdeb35b3dec3c8
Rowan Cockett <rowanc1@gmail.com>
2023-08-11 11:23:48 -0600

    🌄 Improvements to article header (#211)

--- packages/site/src/components/Headers.tsx
+++ packages/site/src/components/Headers.tsx
@@ -21,54 +21,73 @@ export function ArticleHeader({
   const grid = useGridSystemProvider();
   const { subject, venue, biblio, ...rest } = frontmatter ?? {};
   return (
-    <header
-      className={classNames(
-        'w-full relative pt-[2rem] col-screen article',
-        grid,
-        'subgrid-gap',
-        {
-          'bg-no-repeat bg-cover bg-top': frontmatter?.banner,
-          'pb-[4rem]': frontmatter?.banner,
-        },
-        className,
-      )}
-      style={{
-        backgroundImage: frontmatter?.banner ? `url(${frontmatter?.banner})` : undefined,
-      }}
-    >
+    <header className="relative col-screen">
       {frontmatter?.banner && (
-        <div className="absolute border-white shadow-2xl bg-white/80 dark:bg-black/80 backdrop-blur top-[2rem] h-[calc(100%-4rem)] w-full col-screen md:col-screen-inset pointer-events-none"></div>
+        // This is the banner contained in a full-bleed div
+        <div
+          className={classNames(
+            'absolute',
+            grid,
+            'subgrid-gap col-screen bg-no-repeat bg-cover bg-top w-full h-full -z-10 pointer-events-none',
+          )}
+          style={{
+            backgroundImage: `url(${frontmatter?.banner})`,
+          }}
+        >
+          <div
+            className={classNames(
+              'absolute border-white shadow-2xl bg-white/80 dark:bg-black/80 backdrop-blur col-page-right',
+              'w-full',
+              'top-[2rem] h-[calc(100%-4rem)] md:top-[4rem] md:h-[calc(100%-8rem)]',
+            )}
+          />
+        </div>
       )}
       <div
-        className={classNames('flex w-full align-middle z-10 py-2 mb-[1rem] text-sm', {
-          'col-screen md:col-screen-inset px-4': frontmatter?.banner,
-          'col-page-right': !frontmatter?.banner,
-          'bg-white/80 dark:bg-black/80': frontmatter?.banner,
-        })}
-      >
-        {subject && (
-          <div
-            className={classNames('flex-none pr-2 smallcaps', {
-              'border-r mr-2': venue,
-            })}
-          >
-            {subject}
-          </div>
+        className={classNames(
+          'w-full relative col-screen article',
+          grid,
+          'subgrid-gap',
+          {
+            'my-[2rem] pb-[2rem] md:my-[4rem]': frontmatter?.banner,
+            'my-[2rem]': !frontmatter?.banner,
+          },
+          className,
         )}
-        <Journal venue={venue} biblio={biblio} />
-        <div className="flex-grow"></div>
-        <LicenseBadges license={frontmatter?.license} />
-        <OpenAccessBadge open_access={frontmatter?.open_access} />
-        <GitHubLink github={frontmatter?.github} />
+      >
+        <div
+          className={classNames('flex w-full align-middle py-2 mb-[1rem] text-sm', {
+            'col-page-right px-4 w-full': frontmatter?.banner,
+            'bg-white/80 dark:bg-black/80': frontmatter?.banner,
+            'col-page-right': !frontmatter?.banner,
+          })}
+        >
+          {subject && (
+            <div
+              className={classNames('flex-none pr-2 smallcaps', {
+                'border-r mr-2': venue,
+              })}
+            >
+              {subject}
+            </div>
+          )}
+          <Journal venue={venue} biblio={biblio} />
+          <div className="flex-grow"></div>
+          <div className="hidden sm:block">
+            <LicenseBadges license={frontmatter?.license} />
+            <OpenAccessBadge open_access={frontmatter?.open_access} />
+            <GitHubLink github={frontmatter?.github} />
+          </div>
+        </div>
+        <FrontmatterBlock
+          frontmatter={rest}
+          authorStyle="list"
+          className={classNames({ 'pt-4 px-6': frontmatter?.banner })}
+          hideBadges
+          hideExports
+        />
+        {children}
       </div>
-      <FrontmatterBlock
-        frontmatter={rest}
-        authorStyle="list"
-        className={classNames('z-10', { 'pt-4': frontmatter?.banner })}
-        hideBadges
-        hideExports
-      />
-      {children}
     </header>
   );
 }

0a04c86c61b1601222fef49f822926c846a0db55
Rowan Cockett <rowanc1@gmail.com>
2023-08-13 10:52:40 -0600

    🎨 Improve author popover styles (#221)

--- packages/frontmatter/src/AuthorPopover.tsx
+++ packages/frontmatter/src/AuthorPopover.tsx
@@ -30,16 +30,13 @@ export const AuthorPopover = ({
       <Popover.Trigger asChild>
         <button
           className="focus:shadow-[0_0_0_2px] focus:shadow-black outline-none hover:underline"
-          aria-label="Update dimensions"
+          aria-label="Author Details"
         >
           {children}
         </button>
       </Popover.Trigger>
       <Popover.Portal>
-        <Popover.Content
-          className="rounded p-5 w-[400px] bg-white shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
-          sideOffset={5}
-        >
+        <Popover.Content className="rounded p-5 w-[400px] bg-white shadow" sideOffset={5}>
           <div className="flex flex-col gap-2.5">
             <p className="text-mauve12 text-[15px] leading-[19px] font-medium mb-2.5">
               {author.name}

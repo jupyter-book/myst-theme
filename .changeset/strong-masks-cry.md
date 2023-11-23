acc8441a7ee11f4b003afc4e9f3f0cd217d943f3
Rowan Cockett <rowanc1@gmail.com>
2023-11-23 11:41:20 -0700

    🌉 Minor improvements for handling subfigures (#281)
    See executablebooks/mystmd#770

---
+++ .changeset/strong-masks-cry.md
@@ -0,0 +1,7 @@
+---
+'myst-to-react': patch
+'@myst-theme/jupyter': patch
+'@myst-theme/styles': patch
+---
+
+Improve handling for subfigures
--- packages/jupyter/src/figure.tsx
+++ packages/jupyter/src/figure.tsx
@@ -1,6 +1,7 @@
 import { SourceFileKind } from 'myst-spec-ext';
 import { type GenericNode } from 'myst-common';
 import { DEFAULT_RENDERERS, MyST } from 'myst-to-react';
+import classNames from 'classnames';
 import { OutputDecoration } from './decoration.js';
 
 export function Figure({ node }: { node: GenericNode }) {
@@ -11,15 +12,20 @@ export function Figure({ node }: { node: GenericNode }) {
     const placeholder = node.children?.find((child) => child.type === 'image' && child.placeholder);
     const others = node.children?.filter((child) => !(child.type === 'image' && child.placeholder));
     return (
-      <OutputDecoration
-        key={node.key}
-        outputId={output.id}
-        placeholder={placeholder}
-        title={node.source?.title}
-        url={node.source?.url}
+      <figure
+        id={node.html_id || node.identifier || node.key}
+        className={classNames({ subcontainer: node.subcontainer }, node.class)}
       >
-        <MyST ast={others} />
-      </OutputDecoration>
+        <OutputDecoration
+          key={node.key}
+          outputId={output.id}
+          placeholder={placeholder}
+          title={node.source?.title}
+          url={node.source?.url}
+        >
+          <MyST ast={others} />
+        </OutputDecoration>
+      </figure>
     );
   }
   return <Container node={node} />;

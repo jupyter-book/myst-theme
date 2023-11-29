9d614c62b75d1f2ec2d8243159e1c8e4d26a31bc
stevejpurves <steve@curvenote.com>
2023-11-29 09:50:47 +0000

    🧹 remove unneeded code

--- packages/jupyter/src/utils.ts
+++ packages/jupyter/src/utils.ts
@@ -1,62 +1,10 @@
-import type { ManifestProject } from '@myst-theme/providers';
-import type { Thebe, JupyterServerOptions, BinderHubOptions } from 'myst-frontmatter';
-import type { CoreOptions, WellKnownRepoProvider } from 'thebe-core';
+import type { Thebe, JupyterServerOptions } from 'myst-frontmatter';
+import type { CoreOptions } from 'thebe-core';
 
 export type ExtendedCoreOptions = CoreOptions & {
   useBinder?: boolean;
   useJupyterLite?: boolean;
 };
-
-function extractGithubRepoInfo(url: string): { owner: string; repo: string } | null {
-  const pattern = /https?:\/\/github\.com\/([^/]+)\/([^/]+)/;
-  const match = url.match(pattern);
-
-  if (match) {
-    return {
-      owner: match[1],
-      repo: match[2],
-    };
-  }
-
-  return null;
-}
-
-function extractBinderRepoInfo(url: string): {
-  binderUrl: string;
-  repoProvider: WellKnownRepoProvider | string;
-  owner: string;
-  repo: string;
-  ref: string;
-} | null {
-  const pattern = /(https?:\/\/[^/]+(?:\/[^/]+)*?)\/(v\d+)\/([^/]+)\/([^/]+)\/([^/]+)\/([^/]+)/;
-  const match = url.match(pattern);
-
-  if (match) {
-    const repoProviderAbbreviation = match[3];
-    let repoProvider;
-
-    switch (repoProviderAbbreviation) {
-      case 'gh':
-        repoProvider = 'github';
-        break;
-      case 'gl':
-        repoProvider = 'gitlab';
-        break;
-      default:
-        repoProvider = repoProviderAbbreviation;
-    }
-
-    return {
-      binderUrl: match[1],
-      repoProvider: repoProvider,
-      owner: match[4],
-      repo: match[5],
-      ref: match[6],
-    };
-  }
-
-  return null;
-}
 
 function isObject(maybeObject: any) {
   return typeof maybeObject === 'object' && maybeObject !== null;

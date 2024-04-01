41d0171011744c593476924384cdb499b4dd76c9
stevejpurves <steve@curvenote.com>
2024-04-01 17:14:09 +0100

    📐migrate artcle theme meta to V2 and align with book

--- themes/article/app/routes/_index.tsx
+++ themes/article/app/routes/_index.tsx
@@ -1,19 +1,45 @@
-import { ProjectPageCatchBoundary, responseNoArticle, responseNoSite } from '@myst-theme/site';
+import {
+  ProjectPageCatchBoundary,
+  getMetaTagsForArticle,
+  responseNoArticle,
+  responseNoSite,
+} from '@myst-theme/site';
 import Page from './$';
 import { ArticlePageAndNavigation } from '../components/ArticlePageAndNavigation';
 import { getConfig, getPage } from '../utils/loaders.server';
-import type { LoaderFunction } from '@remix-run/node';
+import type { LoaderFunction, V2_MetaFunction } from '@remix-run/node';
 import { redirect } from '@remix-run/node';
+import { SiteManifest } from 'myst-config';
+import { getProject } from '@myst-theme/common';
 export { links } from './$';
+
+type ManifestProject = Required<SiteManifest>['projects'][0];
+
+export const meta: V2_MetaFunction = ({ data, location }) => {
+  if (!data) return [];
+
+  const config: SiteManifest = data.config;
+  const project: ManifestProject = data.project;
+
+  return getMetaTagsForArticle({
+    origin: '',
+    url: location.pathname,
+    title: config?.title ?? project.title,
+    description: config.description ?? project.description ?? undefined,
+    image: (project.thumbnailOptimized || project.thumbnail) ?? undefined,
+    keywords: config.keywords ?? project.keywords ?? [],
+    twitter: config?.options?.twitter,
+  });
+};
 
 export const loader: LoaderFunction = async ({ params, request }) => {
   const config = await getConfig();
   if (!config) throw responseNoSite();
-  const project = config?.projects?.[0];
+  const project = getProject(config);
   if (!project) throw responseNoArticle();
   if (project.slug) return redirect(`/${project.slug}`);
   const page = await getPage(request, { slug: project.index });
-  return page;
+  return { config, project, page };
 };
 
 export default Page;

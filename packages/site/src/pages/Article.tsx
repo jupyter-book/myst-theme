d373fe3d1badc5297cec00ecbdad0aaa2b60dcdc
Angus Hollands <goosey15@gmail.com>
2024-05-15 17:25:31 +0100

    chore: restore export

---
+++ packages/site/src/pages/Article.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { ReferencesProvider, useProjectManifest } from '@myst-theme/providers';
+import {
+  Bibliography,
+  ContentBlocks,
+  FooterLinksBlock,
+  FrontmatterParts,
+  BackmatterParts,
+} from '../components/index.js';
+import { ErrorDocumentNotFound } from './ErrorDocumentNotFound.js';
+import { ErrorProjectNotFound } from './ErrorProjectNotFound.js';
+import type { PageLoader } from '@myst-theme/common';
+import { copyNode, type GenericParent } from 'myst-common';
+import { SourceFileKind } from 'myst-spec-ext';
+import {
+  ExecuteScopeProvider,
+  BusyScopeProvider,
+  NotebookToolbar,
+  ConnectionStatusTray,
+  ErrorTray,
+  useComputeOptions,
+} from '@myst-theme/jupyter';
+import { FrontmatterBlock } from '@myst-theme/frontmatter';
+import { extractKnownParts } from '../utils.js';
+import type { SiteAction } from 'myst-config';
+
+/**
+ * Combines the project downloads and the export options
+ */
+function combineDownloads(
+  siteDownloads: SiteAction[] | undefined,
+  pageFrontmatter: PageLoader['frontmatter'],
+) {
+  if (pageFrontmatter.downloads) {
+    return pageFrontmatter.downloads;
+  }
+  // No downloads on the page, combine the exports if they exist
+  if (siteDownloads) {
+    return [...(pageFrontmatter.exports ?? []), ...siteDownloads];
+  }
+  return pageFrontmatter.exports;
+}
+
+export const ArticlePage = React.memo(function ({
+  article,
+  hide_all_footer_links,
+  hideKeywords,
+}: {
+  article: PageLoader;
+  hide_all_footer_links?: boolean;
+  hideKeywords?: boolean;
+}) {
+  const manifest = useProjectManifest();
+  const compute = useComputeOptions();
+
+  const { hide_title_block, hide_footer_links } = (article.frontmatter as any)?.options ?? {};
+  const downloads = combineDownloads(manifest?.downloads, article.frontmatter);
+  const tree = copyNode(article.mdast);
+  const keywords = article.frontmatter?.keywords ?? [];
+  const parts = extractKnownParts(tree);
+
+  return (
+    <ReferencesProvider
+      references={{ ...article.references, article: article.mdast }}
+      frontmatter={article.frontmatter}
+    >
+      <BusyScopeProvider>
+        <ExecuteScopeProvider enable={compute?.enabled ?? false} contents={article}>
+          {!hide_title_block && (
+            <FrontmatterBlock
+              kind={article.kind}
+              frontmatter={{ ...article.frontmatter, downloads }}
+              className="pt-5 mb-8"
+            />
+          )}
+          {compute?.enabled &&
+            compute.features.notebookCompute &&
+            article.kind === SourceFileKind.Notebook && <NotebookToolbar showLaunch />}
+          {compute?.enabled && article.kind === SourceFileKind.Article && (
+            <ErrorTray pageSlug={article.slug} />
+          )}
+          <div id="skip-to-article" />
+          <FrontmatterParts parts={parts} keywords={keywords} hideKeywords={hideKeywords} />
+          <ContentBlocks pageKind={article.kind} mdast={tree as GenericParent} />
+          <BackmatterParts parts={parts} />
+          <Bibliography />
+          <ConnectionStatusTray />
+          {!hide_footer_links && !hide_all_footer_links && (
+            <FooterLinksBlock links={article.footer} />
+          )}
+        </ExecuteScopeProvider>
+      </BusyScopeProvider>
+    </ReferencesProvider>
+  );
+});
+
+export function ProjectPageCatchBoundary() {
+  return <ErrorProjectNotFound />;
+}
+
+export function ArticlePageCatchBoundary() {
+  return <ErrorDocumentNotFound />;
+}
--- packages/site/src/pages/index.ts
+++ packages/site/src/pages/index.ts
@@ -1,4 +1,5 @@
 export { ErrorProjectNotFound } from './ErrorProjectNotFound.js';
 export { ErrorDocumentNotFound } from './ErrorDocumentNotFound.js';
 export { Error404 } from './Error404.js';
+export { ArticlePage, ArticlePageCatchBoundary, ProjectPageCatchBoundary } from './Article.js';
 export { App, Document, AppCatchBoundary } from './Root.js';

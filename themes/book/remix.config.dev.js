2040428e31d8402348610754d5840c8e9a62fdf2
Angus Hollands <goosey15@gmail.com>
2024-06-20 16:14:49 +0100

    fix:drop feature flags, add polyfills

--- themes/book/remix.config.dev.js
+++ themes/book/remix.config.dev.js
@@ -1,3 +1,5 @@
+const { builtinModules } = require("node:module");
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   publicPath: '/myst_assets_folder/',
@@ -61,12 +63,8 @@ module.exports = {
     'react-merge-refs',
   ],
   watchPaths: ['../../packages/**/*'],
-  future: {
-    v2_dev: true,
-    v2_routeConvention: true,
-    v2_normalizeFormMethod: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_errorBoundary: true,
+
+  browserNodeBuiltinsPolyfill: {
+    modules: builtinModules,
   },
 };

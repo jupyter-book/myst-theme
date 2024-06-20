c265b98c8bf860a570766d0546777673bd2db352
Angus Hollands <goosey15@gmail.com>
2024-06-20 16:14:49 +0100

    fix:drop feature flags, add polyfills

--- themes/book/remix.config.dev.js
+++ themes/book/remix.config.dev.js
@@ -1,3 +1,5 @@
+const { builtinModules } = require("node:module");
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   publicPath: '/myst_assets_folder/',
@@ -60,12 +62,7 @@ module.exports = {
     'jats-utils',
   ],
   watchPaths: ['../../packages/**/*'],
-  future: {
-    v2_dev: true,
-    v2_routeConvention: true,
-    v2_normalizeFormMethod: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_errorBoundary:true,
+  browserNodeBuiltinsPolyfill: {
+    modules: builtinModules,
   },
 };

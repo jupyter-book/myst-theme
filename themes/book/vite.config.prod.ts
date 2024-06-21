b55cb0d10a87c2bac4167e8135d87a41430ea7f4
Angus Hollands <goosey15@gmail.com>
2024-06-21 15:06:48 +0100

    wip: prod

---
+++ themes/book/vite.config.prod.ts
@@ -0,0 +1,36 @@
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import { installGlobals } from '@remix-run/node';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { envOnlyMacros } from 'vite-env-only';
+
+import { createRoutesFromFolders } from '@remix-run/v1-route-convention';
+
+installGlobals();
+
+export default defineConfig({
+  plugins: [
+    remix({
+      basename: process.env.BASE_URL ?? '/',
+      serverBuildFile: 'index.js',
+      ignoredRouteFiles: ['**/.*'],
+      serverModuleFormat: 'esm',
+      routes(defineRoutes) {
+        // uses the v1 convention, works in v1.15+ and v2
+        return createRoutesFromFolders(defineRoutes);
+      },
+    }),
+    tsconfigPaths(),
+    envOnlyMacros(),
+  ],
+  base: (process.env.BASE_URL ?? '/') + 'myst_assets_folder/',
+  resolve: {
+    mainFields: ['main', 'module'],
+  },
+  build: {
+    minify: false,
+  },
+  ssr: {
+    noExternal: [/.*/],
+  },
+});

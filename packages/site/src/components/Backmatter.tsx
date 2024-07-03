de79758a925d77e62bcc78b514e0e11d736d5c5b
Steve Purves <steve@curvenote.com>
2024-07-03 15:52:13 +0100

    💉 adding className props (#419)
    * 💉 adding className props
    
    * 🔧 optional classNames

--- packages/site/src/components/Backmatter.tsx
+++ packages/site/src/components/Backmatter.tsx
@@ -1,17 +1,32 @@
 import type { GenericParent } from 'myst-common';
 import { HashLink, MyST } from 'myst-to-react';
 import { getChildren, type KnownParts } from '../utils.js';
+import classNames from 'classnames';
 
-export function BackmatterParts({ parts }: { parts: KnownParts }) {
+export function BackmatterParts({
+  parts,
+  containerClassName,
+  innerClassName,
+}: {
+  parts: KnownParts;
+  containerClassName?: string;
+  innerClassName?: string;
+}) {
   return (
-    <>
-      <Backmatter title="Acknowledgments" id="acknowledgments" content={parts.acknowledgments} />
+    <div className={containerClassName}>
       <Backmatter
+        className={innerClassName}
+        title="Acknowledgments"
+        id="acknowledgments"
+        content={parts.acknowledgments}
+      />
+      <Backmatter
+        className={innerClassName}
         title="Data Availability"
         id="data-availability"
         content={parts.data_availability}
       />
-    </>
+    </div>
   );
 }
 
@@ -19,14 +34,16 @@ export function Backmatter({
   title,
   id,
   content,
+  className,
 }: {
   title: string;
   id: string;
   content?: GenericParent;
+  className?: string;
 }) {
   if (!content) return null;
   return (
-    <div className="flex flex-col w-full md:flex-row group/backmatter">
+    <div className={classNames('flex flex-col w-full md:flex-row group/backmatter', className)}>
       <h2
         id={id}
         className="mt-5 text-base font-semibold group md:w-[200px] self-start md:flex-none opacity-90 group-hover/backmatter:opacity-100"

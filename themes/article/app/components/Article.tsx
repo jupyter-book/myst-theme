56a28887947c0863f4d76e630a142369480abab6
stevejpurves <steve@curvenote.com>
2024-07-03 16:35:44 +0100

    ✨ 2px

--- themes/article/app/components/Article.tsx
+++ themes/article/app/components/Article.tsx
@@ -17,7 +17,7 @@ import { copyNode } from 'myst-common';
 import { BusyScopeProvider, ConnectionStatusTray, ExecuteScopeProvider } from '@myst-theme/jupyter';
 import { SourceFileKind } from 'myst-spec-ext';
 
-const TOP_OFFSET = 26;
+const TOP_OFFSET = 24;
 
 export function Article({
   article,
@@ -52,7 +52,7 @@ export function Article({
               className="block my-10 lg:sticky lg:top-0 lg:z-10 lg:h-0 lg:pt-0 lg:my-0 lg:ml-10 lg:col-margin-right"
               style={{ top: top + TOP_OFFSET }}
             >
-              <DocumentOutline className="relative" maxdepth={outlineMaxDepth}>
+              <DocumentOutline className="relative pt-[2px]" maxdepth={outlineMaxDepth}>
                 <SupportingDocuments />
               </DocumentOutline>
             </div>

3bc9461b1da24255ea6a120b039c5658b72ed8e4
Angus Hollands <goosey15@gmail.com>
2024-07-30 17:11:17 +0100

    fix: disable wrapping of navbar, disable shrinkage

--- packages/site/src/components/Navigation/TopNav.tsx
+++ packages/site/src/components/Navigation/TopNav.tsx
@@ -150,8 +150,8 @@ export function TopNav() {
   const { logo, logo_dark, logo_text } = config?.options ?? {};
   return (
     <div className="bg-white/80 backdrop-blur dark:bg-stone-900/80 shadow dark:shadow-stone-700 p-3 md:px-8 fixed w-screen top-0 z-30 h-[60px]">
-      <nav className="flex items-center justify-between flex-wrap max-w-[1440px] mx-auto">
-        <div className="flex flex-row xl:min-w-[19.5rem] mr-2 sm:mr-7 justify-start items-center">
+      <nav className="flex items-center justify-between flex-nowrap max-w-[1440px] mx-auto">
+        <div className="flex flex-row xl:min-w-[19.5rem] mr-2 sm:mr-7 justify-start items-center shrink-0">
           <div className="block xl:hidden">
             <button
               className="flex items-center border-stone-400 text-stone-800 hover:text-stone-900 dark:text-stone-200 hover:dark:text-stone-100"

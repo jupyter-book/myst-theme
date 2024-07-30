a7ef6a3a75913f0c63433256956dd4900466ac9e
Angus Hollands <goosey15@gmail.com>
2024-07-30 17:48:39 +0100

    fix: use sticky rather than fixed
    This allows us to scroll

--- packages/site/src/components/Navigation/TopNav.tsx
+++ packages/site/src/components/Navigation/TopNav.tsx
@@ -149,7 +149,7 @@ export function TopNav() {
   const { title, nav, actions } = config ?? {};
   const { logo, logo_dark, logo_text } = config?.options ?? {};
   return (
-    <div className="bg-white/80 backdrop-blur dark:bg-stone-900/80 shadow dark:shadow-stone-700 p-3 md:px-8 fixed w-screen top-0 z-30 h-[60px]">
+    <div className="bg-white/80 backdrop-blur dark:bg-stone-900/80 shadow dark:shadow-stone-700 p-3 md:px-8 sticky w-screen top-0 z-30 h-[60px]">
       <nav className="flex items-center justify-between flex-nowrap max-w-[1440px] mx-auto">
         <div className="flex flex-row xl:min-w-[19.5rem] mr-2 sm:mr-7 justify-start items-center shrink-0">
           <div className="block xl:hidden">

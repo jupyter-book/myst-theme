d453e0a6e997db357c6c853fe14b780c754994bd
stevejpurves <steve@curvenote.com>
2024-09-09 11:14:42 +0100

    ✨🔨 update patches

--- packages/myst-to-react/types/declarations.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'react-syntax-highlighter/dist/esm/styles/hljs/xcode.js' {
-  const content: any;
-  export default content;
-}
-
-declare module 'react-syntax-highlighter/dist/esm/styles/hljs/vs2015.js' {
-  const content: any;
-  export default content;
-}

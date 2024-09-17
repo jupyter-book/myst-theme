c19d401bbaf315f09a49d1f8ddb340b7aa013fab
Angus Hollands <goosey15@gmail.com>
2024-09-17 11:22:23 +0100

    refactor: small changes

--- packages/myst-to-react/src/icon.tsx
+++ packages/myst-to-react/src/icon.tsx
@@ -3,12 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core';
 
 // Workaround to problem with icons being huge huge first load prior to resizing
 // This ensures that the icon CSS is loaded immediately before attempting to render icons
 // See https://github.com/FortAwesome/react-fontawesome/issues/134
-import { config as fontAwesomeConfig } from '@fortawesome/fontawesome-svg-core';
+import { library, config as fontAwesomeConfig } from '@fortawesome/fontawesome-svg-core';
+
 // Prevent fontawesome from dynamically adding its css since we did it manually above
 fontAwesomeConfig.autoAddCss = false;
 fontAwesomeConfig.autoReplaceSvg = false;
@@ -32,10 +32,12 @@ export const IconRenderer: NodeRenderer<IconSpec> = ({ node }) => {
   switch (node.kind) {
     case 'fas':
     case 'far':
-    case 'fab':
-      const icon = icons.get(`${node.kind}-${node.name}`);
+    case 'fab': {
+      const icon = icons.get(`${node.kind}-${node.name}`) ?? fas.faCircleExclamation;
+      console.log(`${node.kind}-${node.name}`);
 
-      return <FontAwesomeIcon icon={icon ?? fas.faCircleExclamation} />;
+      return <FontAwesomeIcon icon={icon} />;
+    }
     default:
       return <FontAwesomeIcon icon={fas.faCircleExclamation} />;
   }

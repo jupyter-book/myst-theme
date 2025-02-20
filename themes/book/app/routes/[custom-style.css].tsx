2e7bfd263d7749afc9d17295e7fc34d76dea230b
Angus Hollands <goosey15@gmail.com>
2025-02-20 14:17:43 +0000

    fix: don't 404 for missing custom styles?

--- themes/book/app/routes/[custom-style.css].tsx
+++ themes/book/app/routes/[custom-style.css].tsx
@@ -3,9 +3,14 @@ import { getCustomStyleSheet } from '~/utils/loaders.server';
 
 export const loader: LoaderFunction = async (): Promise<Response> => {
   const css = await getCustomStyleSheet();
-  if (!css) return new Response('No custom CSS stylesheet found', { status: 404 });
-  return new Response(
-    css.buffer,
-    css.contentType ? { headers: { 'Content-Type': css.contentType } } : undefined,
-  );
+  if (!css) {
+    return new Response(`/* No custom stylesheet found */`, {
+      headers: { 'Content-Type': 'text/css' },
+    });
+  } else {
+    return new Response(
+      css.buffer,
+      css.contentType ? { headers: { 'Content-Type': css.contentType } } : undefined,
+    );
+  }
 };

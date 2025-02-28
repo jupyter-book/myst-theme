aba17f00d2c68389acebdc7d6cb4c7934f63defb
Rowan Cockett <rowanc1@gmail.com>
2025-02-27 19:06:32 -0700

    Deprecate Block from site package.

--- packages/site/src/components/ContentBlocks.tsx
+++ packages/site/src/components/ContentBlocks.tsx
@@ -1,14 +1,12 @@
-import { SourceFileKind } from 'myst-spec-ext';
+import type { SourceFileKind } from 'myst-spec-ext';
 import type { GenericParent } from 'myst-common';
-import { MyST } from 'myst-to-react';
-export { Block } from 'myst-to-react';
+import { MyST, Block as MystBlock } from 'myst-to-react';
 
 /**
  * @deprecated This component is not maintained, in favor of the generic `MyST` component
  */
 export function ContentBlocks({
   mdast,
-  pageKind = SourceFileKind.Article,
   className,
 }: {
   mdast: GenericParent;
@@ -17,3 +15,6 @@ export function ContentBlocks({
 }) {
   return <MyST ast={mdast} className={className} />;
 }
+
+/** @deprecated use `import { Block } from 'myst-to-react';` */
+export const Block = MystBlock;

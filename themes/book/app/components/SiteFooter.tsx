a08bcf383462c84ef486213376590f2383bb73df
Stefan van der Walt <stefanv@berkeley.edu>
2025-05-12 10:58:53 -0700

    Use footer-description and footer-column blocks

--- themes/book/app/components/SiteFooter.tsx
+++ themes/book/app/components/SiteFooter.tsx
@@ -1,16 +1,12 @@
-import { MastodonIcon, TwitterIcon, GithubIcon, DiscordIcon } from '@scienceicons/react/24/solid';
-import { Link } from '@remix-run/react';
+import type { GenericNode, GenericParent } from 'myst-common';
 import classNames from 'classnames';
-import { extractPart } from 'myst-common';
 import { MyST } from 'myst-to-react';
-import { select, selectAll, matches } from 'unist-util-select';
-import { filter } from 'unist-util-filter';
+import { select, selectAll } from 'unist-util-select';
 
 
-export function SiteFooter({ tight, content }: { tight?: boolean }) {
-  const footerBlock = selectAll('block', content).find((node) => node.kind == 'footer');
-  const footerColumnsBlock = selectAll('block', content).find((node) => node.kind == 'footer-columns');
-  const footerLists = selectAll('list', footerColumnsBlock);
+export function SiteFooter({ tight, content }: { tight?: boolean, content: GenericParent }) {
+  const footerBlock = select('block[kind=footer-description]', content) as GenericNode | undefined;
+  const footerColumnBlocks = selectAll('block[kind=footer-column]', content) as GenericParent[];
 
   return (
     <section className={classNames('col-screen article-grid', { 'mt-10': !tight, 'mt-0': tight })}>
@@ -23,11 +19,16 @@ export function SiteFooter({ tight, content }: { tight?: boolean }) {
         <div className="my-10 col-page">
           <div className="flex flex-col items-center w-full p-2 my-4 sm:p-5 lg:flex-row lg:flex-wrap">
             <div className="max-w-[200px]">
-              <MyST ast={footerBlock}/>
+              { footerBlock && <MyST ast={footerBlock}/> }
             </div>
             <div className="hidden grow lg:block"></div>
             <div className="flex flex-row">
-              {footerLists.map((lst, i) => <MyST ast={lst} key={i} className="list-none mr-10 leading-loose lg:px-4"/>)}
+              {footerColumnBlocks.map((blk, i) =>
+                <MyST
+                  ast={blk.children}
+                  key={i}
+                  className={classNames(blk.class, "list-none mr-10 leading-loose lg:px-4")}/>
+              )}
             </div>
 
           </div>

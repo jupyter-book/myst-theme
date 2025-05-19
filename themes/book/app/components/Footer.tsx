5f16b1124ca1a1e595581fef30b1931a77d45c12
Angus Hollands <goosey15@gmail.com>
2025-05-19 16:24:49 +0100

    refactor: drop extra div

--- themes/book/app/components/Footer.tsx
+++ themes/book/app/components/Footer.tsx
@@ -5,10 +5,14 @@ import { MyST } from 'myst-to-react';
 export function Footer({ content, className }: { content: GenericParent; className?: string }) {
   return (
     // Outer footer, sets up the grid, leaves margin above
-    <footer className={classNames('article footer article-grid bg-white dark:bg-slate-950 mt-10')}>
-      <div className={classNames(className, 'article-grid col-screen shadow-2xl shadow py-10')}>
-        <MyST ast={content} />
-      </div>
+    <footer
+      className={classNames(
+        'article footer article-grid bg-white dark:bg-slate-950 mt-10 shadow-2xl shadow py-10',
+        className,
+      )}
+    >
+      {/* Inner div that adds the shadow */}
+      <MyST ast={content} />
     </footer>
   );
 }

2099b574ca5372f2117e4a881c7603fe4e8ad067
artoftheblue <91217360+artoftheblue@users.noreply.github.com>
2025-06-07 16:25:33 +0300

    add custom CSS markers to dropdowns

--- packages/myst-to-react/src/dropdown.tsx
+++ packages/myst-to-react/src/dropdown.tsx
@@ -32,7 +32,7 @@ export function Details({
   return (
     <details
       className={classNames(
-        'rounded-md my-5 shadow dark:shadow-2xl dark:shadow-neutral-900 overflow-hidden',
+        '-dropdown-style rounded-md my-5 shadow dark:shadow-2xl dark:shadow-neutral-900 overflow-hidden',
         'bg-gray-50 dark:bg-stone-800',
         className,
       )}
@@ -40,12 +40,12 @@ export function Details({
     >
       <summary
         className={classNames(
-          'm-0 text-lg font-medium py-1 min-h-[2em] pl-3',
+          '-dropdown-header m-0 text-lg font-medium py-1 min-h-[2em] pl-3',
           'cursor-pointer hover:shadow-[inset_0_0_0px_30px_#00000003] dark:hover:shadow-[inset_0_0_0px_30px_#FFFFFF03]',
           'bg-gray-100 dark:bg-slate-900',
         )}
       >
-        <span className="text-neutral-900 dark:text-white">
+        <span className="-dropdown-header-title text-neutral-900 dark:text-white">
           <span className="block float-right text-sm font-thin text-neutral-700 dark:text-neutral-200">
             <ChevronRightIcon
               width="1.5rem"
@@ -56,7 +56,7 @@ export function Details({
           {title}
         </span>
       </summary>
-      <div className="px-4 py-1 details-body">{children}</div>
+      <div className="-dropdown-body px-4 py-1 details-body">{children}</div>
     </details>
   );
 }

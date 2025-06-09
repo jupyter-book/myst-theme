bb2bd39ed9bddd9364a5b33f79d02996c4ad8b5e
artoftheblue <91217360+artoftheblue@users.noreply.github.com>
2025-06-09 21:11:38 +0300

    remove period from light/dark mode switch hover text

--- packages/site/src/components/Navigation/ThemeButton.tsx
+++ packages/site/src/components/Navigation/ThemeButton.tsx
@@ -11,8 +11,8 @@ export function ThemeButton({ className = 'w-8 h-8 mx-3' }: { className?: string
         'theme rounded-full aspect-square border border-stone-700 dark:border-white hover:bg-neutral-100 border-solid overflow-hidden text-stone-700 dark:text-white hover:text-stone-500 dark:hover:text-neutral-800',
         className,
       )}
-      title={`Toggle theme between light and dark mode.`}
-      aria-label={`Toggle theme between light and dark mode.`}
+      title={`Toggle theme between light and dark mode`}
+      aria-label={`Toggle theme between light and dark mode`}
       onClick={nextTheme}
     >
       <MoonIcon className="h-full w-full p-0.5 hidden dark:block" />

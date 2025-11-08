92b8dc13319fbd2f921f62c37a2e55f26ff863bd
choldgraf <choldgraf@gmail.com>
2025-11-08 13:08:57 -0800

    Re-fix bug for hidden play button

--- packages/jupyter/src/block.tsx
+++ packages/jupyter/src/block.tsx
@@ -27,7 +27,8 @@ export function NotebookBlock({ node, className }: { node: GenericParent; classN
               <NotebookRunCellSpinnerOnly id={`spinner-${node.key}`} />
             </div>
           </div>
-          <div className="hidden sticky top-[80px] z-10 opacity-70 group-hover/block:opacity-100 group-hover/block:flex">
+          {/* Use opacity-0 instead of hidden to keep buttons in DOM layout.*/}
+          <div className="sticky top-[80px] z-10 opacity-0 group-hover/block:opacity-100 group-hover/block:flex">
             <div className="absolute top-0 -right-[28px] flex md:flex-col">
               <NotebookRunCell id={`run-${node.key}`} />
               <NotebookClearCell id={`clear-${node.key}`} />

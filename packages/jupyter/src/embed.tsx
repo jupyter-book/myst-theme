556f6228ea4edb71a3a54c6fa63c06143b92f01e
Steve Purves <steve@curvenote.com>
2025-12-03 16:59:42 +0000

    🌡️ support `outputs` in `embed` and `figure` with temp fallback for legacy output AST from `refs`

--- packages/jupyter/src/embed.tsx
+++ packages/jupyter/src/embed.tsx
@@ -1,19 +1,40 @@
 import { type GenericNode } from 'myst-common';
 import { OutputDecoration } from './decoration.js';
 import { MyST } from 'myst-to-react';
+import { OutputsContextProvider } from './providers.js';
+
+type NodeWithId = GenericNode & { id: string };
 
 export function Embed({ node }: { node: GenericNode }) {
-  const output = node.children?.find((child) => child.type === 'output');
-  if (!output) return <MyST ast={node.children} />;
-  return (
-    <OutputDecoration
-      key={node.key}
-      outputId={output.id ?? output.key}
-      title={node.source?.title}
-      url={node.source?.url}
-      remoteBaseUrl={node.source?.remoteBaseUrl}
-    >
-      <MyST ast={node.children} />
-    </OutputDecoration>
-  );
+  const output = node.children?.find((child) => child.type === 'output') as NodeWithId | undefined;
+  const outputs = node.children?.find((child) => child.type === 'outputs') as
+    | NodeWithId
+    | undefined;
+
+  if (outputs) {
+    return (
+      <OutputDecoration
+        key={node.key}
+        outputId={outputs.id ?? outputs.key}
+        title={node.source?.title}
+        url={node.source?.url}
+        remoteBaseUrl={node.source?.remoteBaseUrl}
+      >
+        <OutputsContextProvider allSafe={false} outputsId={outputs.id ?? outputs.key}>
+          <MyST ast={node.children} />
+        </OutputsContextProvider>
+      </OutputDecoration>
+    );
+  } else if (output) {
+    return (
+      <div className="border border-red-500 p-1">
+        <details className="cursor-pointer">
+          <summary>Legacy Output Embedded</summary>
+          <pre>{JSON.stringify(output, null, 2)}</pre>
+        </details>
+      </div>
+    );
+  }
+
+  return <MyST ast={node.children} />;
 }
--- packages/jupyter/src/figure.tsx
+++ packages/jupyter/src/figure.tsx
@@ -4,30 +4,52 @@ import { DEFAULT_RENDERERS, MyST } from 'myst-to-react';
 import classNames from 'classnames';
 import { OutputDecoration } from './decoration.js';
 
+type NodeWithId = GenericNode & { id: string };
+
 export function Figure({ node }: { node: GenericNode }) {
   const { base: Container } = DEFAULT_RENDERERS['container'];
   const isFromJupyer = node.source?.kind === SourceFileKind.Notebook;
-  const output = node.children?.find((child) => child.type === 'output');
-  if (isFromJupyer && !!output) {
-    const placeholder = node.children?.find((child) => child.type === 'image' && child.placeholder);
-    const others = node.children?.filter((child) => !(child.type === 'image' && child.placeholder));
-    return (
-      <figure
-        id={node.html_id || node.identifier || node.key}
-        className={classNames('myst-jp-figure', { subcontainer: node.subcontainer }, node.class)}
-      >
-        <OutputDecoration
-          key={node.key}
-          outputId={output.id ?? output.key}
-          placeholder={placeholder}
-          title={node.source?.title}
-          url={node.source?.url}
-          remoteBaseUrl={node.source?.remoteBaseUrl}
+
+  const output = node.children?.find((child) => child.type === 'output') as NodeWithId | undefined;
+  const outputs = node.children?.find((child) => child.type === 'outputs') as
+    | NodeWithId
+    | undefined;
+
+  if (isFromJupyer) {
+    if (outputs) {
+      const placeholder = node.children?.find(
+        (child) => child.type === 'image' && child.placeholder,
+      );
+      const others = node.children?.filter(
+        (child) => !(child.type === 'image' && child.placeholder),
+      );
+      return (
+        <figure
+          id={node.html_id || node.identifier || node.key}
+          className={classNames('myst-jp-figure', { subcontainer: node.subcontainer }, node.class)}
         >
-          <MyST ast={others} />
-        </OutputDecoration>
-      </figure>
-    );
+          <OutputDecoration
+            key={node.key}
+            outputId={outputs.id ?? outputs.key}
+            placeholder={placeholder}
+            title={node.source?.title}
+            url={node.source?.url}
+            remoteBaseUrl={node.source?.remoteBaseUrl}
+          >
+            <MyST ast={others} />
+          </OutputDecoration>
+        </figure>
+      );
+    } else if (output) {
+      return (
+        <div className="border border-gred-500">
+          <details>
+            <summary>Legacy Output Embedded</summary>
+            <pre>{JSON.stringify(output, null, 2)}</pre>
+          </details>
+        </div>
+      );
+    }
   }
   return <Container node={node} />;
 }

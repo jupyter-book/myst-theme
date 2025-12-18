5d80fa5ac78c8cf05e516ceee948eebf41c537f6
Angus Hollands <goosey15@gmail.com>
2025-12-18 11:52:22 +0000

    feat: support AST rendering

--- packages/jupyter/src/output.tsx
+++ packages/jupyter/src/output.tsx
@@ -17,10 +17,7 @@ export const DIRECT_MIME_TYPES = new Set([
   KnownCellOutputMimeTypes.ImageGif,
   KnownCellOutputMimeTypes.ImageJpeg,
   KnownCellOutputMimeTypes.ImageBmp,
-  // TODO: handle variants here
-  'text/markdown',
-]) as Set<string>;
-
+]) as Set<string | RegExp>;
 export function isOutputSafe(
   output: MinifiedOutput | null | undefined,
   directOutputTypes: Set<string>,
@@ -67,14 +64,21 @@ export function validateOutput(output: unknown): {
 export function Output({ node, className }: { node: GenericNode; className?: string }) {
   const { outputsId } = useOutputsContext();
   const { ready } = useCellExecution(outputsId ?? '');
-  // FUTURE: we'll be rendering AST outputs directly in future
-  const maybeSafeOutput = useMemo(() => node.jupyter_data, [node]);
-  const isSafe = isOutputSafe(maybeSafeOutput, DIRECT_OUTPUT_TYPES, DIRECT_MIME_TYPES);
 
-  const validation = validateOutput(maybeSafeOutput);
-  if (!outputsId || !maybeSafeOutput || !validation.isValid) {
-    if (validation.reason) {
-      console.error(validation.reason);
+  // Legacy direct React-only rendering path logic
+  const maybeJupyterData = useMemo(() => node.jupyter_data, [node]);
+  const jupyterDataValidation = validateOutput(maybeJupyterData);
+  const jupyterDataIsDirect = isOutputSafe(
+    maybeJupyterData,
+    DIRECT_OUTPUT_TYPES,
+    DIRECT_MIME_TYPES,
+  );
+  // Do we need to abort?
+  const canRenderDirectly =
+    (node.children?.length ?? 0) > 1 || (maybeJupyterData && jupyterDataValidation.isValid);
+  if (!outputsId || !canRenderDirectly) {
+    if (jupyterDataValidation.reason) {
+      console.error(jupyterDataValidation.reason);
     }
     return (
       <Callout
@@ -86,21 +90,29 @@ export function Output({ node, className }: { node: GenericNode; className?: str
       >
         <div className="py-2 space-y-1">
           <div>Output data package is not compatible with the current renderer.</div>
-          {validation.reason && (
-            <div className="font-mono text-sm">Reason: {validation.reason}</div>
+          {jupyterDataValidation.reason && (
+            <div className="font-mono text-sm">Reason: {jupyterDataValidation.reason}</div>
           )}
         </div>
       </Callout>
     );
   }
-
-  if (isSafe && !ready) {
+  // New AST rendering path, always preferred unless there's a kernel involved
+  // TODO: perhaps an opt-out of the ready check here for cells that we want
+  //       to be sticky?
+  if (!ready && node.children?.length) {
+    return <MyST ast={node.children} />;
+  }
+  // Legacy direct rendering path, for not-ready cases
+  else if (jupyterDataIsDirect && !ready) {
     if (node.children?.length) {
       return <MyST ast={node.children} />;
     }
-    return <SafeOutput output={maybeSafeOutput} />;
+    return <SafeOutput output={maybeJupyterData} />;
   }
-
-  // TODO: myst-jp-output should be added to the first child div
-  return <JupyterOutput outputsId={outputsId} output={maybeSafeOutput} />;
+  // Kernel and/or widget backed rendering
+  else {
+    // TODO: myst-jp-output should be added to the first child div
+    return <JupyterOutput outputsId={outputsId} output={maybeJupyterData} />;
+  }
 }

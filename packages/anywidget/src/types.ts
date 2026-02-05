3969aff849545d4f3861ca6c7a633c9d20b322db
Steve Purves <steve@curvenote.com>
2026-02-05 09:56:43 +0000

    🚀 init `anywidget` package

---
+++ packages/anywidget/src/types.ts
@@ -0,0 +1,17 @@
+export type AnyWidgetDirective = {
+  /** The type of the directive */
+  type: 'anywidget';
+  /** The data to pass to the model */
+  data: {
+    /** The ES module to import */
+    esm: string;
+    /** The JSON data to initialize the widget */
+    json: Record<string, unknown>;
+    /** URL to a css stylesheet to load for the widget */
+    css?: string;
+    /** Tailwind classes to apply to the container element */
+    class?: string;
+    /** A static file path, folder path or glob pattern to static files to make available to the module */
+    static?: string;
+  };
+};

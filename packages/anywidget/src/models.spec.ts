56f1692de22e943b5bf181351d658b9cb369f378
Steve Purves <steve@curvenote.com>
2026-02-05 22:12:46 +0000

    🔧 fix test

---
+++ packages/anywidget/src/models.spec.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { describe, it } from 'vitest';
+
+describe('MystAnyModel', () => {
+  it.todo('Develop a test suite for the MystAnyModel class as it is implemented');
+});

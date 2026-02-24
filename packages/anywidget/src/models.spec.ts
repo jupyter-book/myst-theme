68b406f82853cadb4f23c350d3d73ec274b97652
choldgraf <choldgraf@gmail.com>
2026-02-24 15:37:30 -0800

    Add a simple test

--- packages/anywidget/src/models.spec.ts
+++ packages/anywidget/src/models.spec.ts
@@ -1,6 +1,20 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { describe, it } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
+import { MystAnyModel } from './models.js';
 
 describe('MystAnyModel', () => {
-  it.todo('Develop a test suite for the MystAnyModel class as it is implemented');
+  // Test the core widget data flow set/get state and fire on changes.
+  it('updates state and fires change event', () => {
+    const model = new MystAnyModel({ a: 0, b: 0 });
+    // This function changes *b* as a callback when *a* changes
+    const cb = vi.fn(() => model.set('b', 10));
+    model.on('change:a', cb);
+
+    // Set a value, which should trigger the callback above
+    model.set('a', 5);
+
+    expect(model.get('a')).toBe(5);
+    expect(model.get('b')).toBe(10);
+    expect(cb).toHaveBeenCalledTimes(1);
+  });
 });

2548ef6aa5383fd975778b33c2f7529a5e5fc165
Stefan van der Walt <stefanv@berkeley.edu>
2026-03-20 08:56:55 -0700

    Render `grid-item`, taking `columns` into consideration (#838)
    Co-authored-by: Chris Holdgraf <choldgraf@gmail.com>

---
+++ .changeset/cute-hounds-drum.md
@@ -0,0 +1,7 @@
+---
+'myst-to-react': minor
+'myst-demo': minor
+'@myst-theme/styles': minor
+---
+
+Add grid-item renderer
--- styles/index.js
+++ styles/index.js
@@ -207,6 +207,12 @@ const safeList = [
   'col-span-4',
   'col-span-5',
   'col-span-6',
+  'col-span-7',
+  'col-span-8',
+  'col-span-9',
+  'col-span-10',
+  'col-span-11',
+  'col-span-12',
   // Utilities
   'shaded',
   'framed',

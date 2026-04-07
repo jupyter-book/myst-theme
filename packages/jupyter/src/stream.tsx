937894a3bb27fb222dcb809d64598a3db1f492a1
Angus Hollands <goosey15@gmail.com>
2026-04-07 14:50:25 +0100

    fix: use error styling for stderr

--- packages/jupyter/src/stream.tsx
+++ packages/jupyter/src/stream.tsx
@@ -4,6 +4,8 @@ import type { MinifiedStreamOutput } from 'nbtx';
 import { MaybeLongContent } from './components.js';
 import { useIsScrollable } from '@myst-theme/providers';
 
+import classNames from 'classnames';
+
 export default function Stream({ output }: { output: MinifiedStreamOutput }) {
   const { ref, isScrollable } = useIsScrollable<HTMLPreElement>();
 
@@ -17,7 +19,9 @@ export default function Stream({ output }: { output: MinifiedStreamOutput }) {
           tabIndex={isScrollable ? 0 : undefined}
           role={isScrollable ? 'region' : undefined}
           aria-label="cell output stream"
-          className="myst-jp-stream-output text-sm font-thin font-system overflow-auto p-3"
+          className={classNames('myst-jp-stream-output text-sm font-thin font-system p-3', {
+            'jupyter-error': output.name === 'stderr',
+          })}
         >
           <Ansi>{content ?? ''}</Ansi>
         </pre>

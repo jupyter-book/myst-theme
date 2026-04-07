08884102cb8ae04fb58fc4d4f704c5b6bff61e25
Silas Santini <70163606+pancakereport@users.noreply.github.com>
2026-04-07 05:31:53 -0700

    Fix scrollable region focusable (code/code output) (#849)
    * scrollable region focusable
    
    * scrollable region focusable cell output
    
    * linting
    
    * Add a couple of examples for wide code
    
    * print'
    
    * Re-use one function for scrollable
    
    * example of a wide table
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: choldgraf <choldgraf@gmail.com>
    Co-authored-by: Angus Hollands <goosey15@gmail.com>

--- packages/jupyter/src/error.tsx
+++ packages/jupyter/src/error.tsx
@@ -2,19 +2,26 @@ import Ansi from '@curvenote/ansi-to-react';
 import { ensureString } from 'nbtx';
 import type { MinifiedErrorOutput } from 'nbtx';
 import { MaybeLongContent } from './components.js';
+import { useIsScrollable } from '@myst-theme/providers';
 
 export default function Error({ output }: { output: MinifiedErrorOutput }) {
+  const { ref, isScrollable } = useIsScrollable<HTMLPreElement>();
+
   return (
     <MaybeLongContent
       content={ensureString(output.traceback)}
       path={output.path}
-      render={(content?: string) => {
-        return (
-          <pre className="myst-jp-error-output text-sm font-thin font-system jupyter-error">
-            <Ansi>{content ?? ''}</Ansi>
-          </pre>
-        );
-      }}
+      render={(content?: string) => (
+        <pre
+          ref={ref}
+          tabIndex={isScrollable ? 0 : undefined}
+          role={isScrollable ? 'region' : undefined}
+          aria-label="cell error output"
+          className="myst-jp-error-output text-sm font-thin font-system jupyter-error overflow-auto"
+        >
+          <Ansi>{content ?? ''}</Ansi>
+        </pre>
+      )}
     />
   );
 }

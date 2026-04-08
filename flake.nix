ce981b97141906b8da12b60bba615d7f1b7b0ffb
Angus Hollands <goosey15@gmail.com>
2026-04-08 15:16:00 +0100

    feat: update flake

--- flake.nix
+++ flake.nix
@@ -19,6 +19,20 @@
           devShells.default = mkShell {
             buildInputs = [
               pkgs.nodejs_22
+              (pkgs.python3.withPackages (ps:
+                with ps; [
+                  jupyter-server
+                  ipykernel
+                  # Computation modules
+                  jupytext
+                  jupyterlab
+                  altair
+                  matplotlib
+                  plotly
+                  bokeh
+                  vega-datasets
+                  numpy
+                ]))
             ];
           };
         }

d9eb3e7c1047388d4399eb025f320f56612b0f2b
Angus Hollands <goosey15@gmail.com>
2026-07-08 15:02:13 +0100

    wip: switch to vite

--- themes/book/app/utils/hash.ts
+++ themes/book/app/utils/hash.ts
@@ -1,8 +1,15 @@
-import { createHash } from 'crypto';
-
 /**
  * Generates a SHA-256 hash from a string
  */
-export function hashString(content: string): string {
-  return createHash('sha256').update(content).digest('hex');
+export async function hashString(content: string): string {
+  const msgUint8 = new TextEncoder().encode(content); // encode as (utf-8) Uint8Array
+  const hashBuffer = await globalThis.crypto.subtle.digest('SHA-256', msgUint8); // hash the message
+  if (Uint8Array.prototype.toHex) {
+    // Use toHex if supported.
+    return new Uint8Array(hashBuffer).toHex(); // Convert ArrayBuffer to hex string.
+  }
+  // If toHex() is not supported, fall back to an alternative implementation.
+  const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+  return hashHex;
 }

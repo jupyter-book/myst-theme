ca21953ce8c342aa751809bfa84746a8a608dbec
Angus Hollands <goosey15@gmail.com>
2026-07-10 13:05:13 +0100

    refactor: drop session storage for theme
    feat: add render build mode
    
    feat: add render step
    
    feat: add build vs start entrypoint
    
    fix: static build mode
    
    fix: also featch search files, etc.
    
    fix: don't update links
    
    feat: set base
    
    fix: set different env var for ssg
    
    feat: test basename
    
    fix: static pre-rendering mode etc
    
    Revert "fix: static pre-rendering mode etc"
    
    This reverts commit 9e9935a19d55315234f0de0e7586eafed0f0322b.
    
    feat: use MODE
    
    fix: more path fixes
    
    fix: base urls

---
+++ themes/book/react-router.static.config.ts
@@ -0,0 +1,78 @@
+import type { Config } from '@react-router/dev/config';
+
+const CONTENT_CDN = process.env.CONTENT_CDN;
+if (CONTENT_CDN === undefined) {
+  throw new Error('Expected CONTENT_CDN');
+}
+process.env.MODE = 'static';
+
+const response = await fetch(`${CONTENT_CDN}/config.json`);
+const config = await response.json();
+
+/**
+ * Change from a slug such as `folder.subfolder.index` to a URL (`folder/subfolder`).
+ *
+ * @param slug
+ * @returns url
+ */
+function slugToUrl<T extends string | undefined>(slug: T): T {
+  if (slug == null) return undefined as T;
+  return slug.replace(/\.index$/, '').replace(/\./g, '/') as T;
+}
+
+interface Page {
+  slug: string;
+}
+
+function makeRoutes(data: any) {
+  const localProj = data.projects[0];
+  const baseurl = '';
+  const host = '';
+
+  const localProjSlug = localProj.slug ? `/${localProj.slug}` : '';
+  if (localProjSlug) {
+    throw new Error();
+  }
+  const makePath = (path: string) => path;
+  // We need to get the index from a slug page to make remix happy
+  // If this gets from the index, then the site will trigger the wrong render path
+  // And then hydration does not match
+  const siteIndex = baseurl ? `/${localProj.index}` : '';
+  const pages = localProj.pages.filter((page): page is Page => !!(page as any).slug);
+  return [
+    { url: `${host}${localProjSlug}${siteIndex}`, path: makePath('index.html') },
+    ...pages.map((page) => {
+      const pathSubPath = slugToUrl(page.slug);
+      return {
+        url: `${host}${localProjSlug}/${pathSubPath}`,
+        path: makePath(`${pathSubPath}/index.html`),
+      };
+    }),
+    // Download all of the configured JSON
+    {
+      url: `${host}${localProjSlug}/${localProj.index}.json`,
+      path: makePath(`${localProj.index}.json`),
+    },
+    ...pages.map((page) => {
+      return {
+        url: `${host}${localProjSlug}/${page.slug}.json`,
+        path: makePath(`${page.slug}.json`),
+      };
+    }),
+    // Download other assets
+    ...['myst.search.json', 'myst.xref.json'].map((asset) => ({
+      url: `${host}/${asset}`,
+      path: asset,
+    })),
+  ].flat();
+}
+export default {
+  ssr: false,
+  basename: process.env.BASE_URL??'/',
+  async prerender({ getStaticPaths }) {
+    return [...makeRoutes(config).map((r) => (r.url ? r.url : '/')), ...getStaticPaths()];
+  },
+  future: {
+    v8_passThroughRequests: true,
+  },
+} satisfies Config;

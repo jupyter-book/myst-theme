8418cf6b1af9c508f9d82b80002f4f0558d1ab42
Angus Hollands <goosey15@gmail.com>
2026-08-04 15:41:04 +0100

    wip: upgrade root

--- themes/article/app/root.tsx
+++ themes/article/app/root.tsx
@@ -1,6 +1,6 @@
-import type { LinksFunction, LoaderFunction, MetaFunction } from '@remix-run/node';
-import tailwind from '~/styles/app.css';
-import thebeCoreCss from 'thebe-core/dist/lib/thebe-core.css';
+import type { LinksFunction, MetaFunction, LoaderFunction } from 'react-router';
+import tailwind from '~/styles/app.css?url';
+import thebeCoreCss from 'thebe-core/dist/lib/thebe-core.css?url';
 import { getConfig } from '~/utils/loaders.server';
 import { type SiteLoader } from '@myst-theme/common';
 import {
@@ -25,11 +25,11 @@ const RENDERERS: NodeRenderers = mergeRenderers([
   ANY_RENDERERS,
 ]);
 
-export const meta: MetaFunction<typeof loader> = ({ data }) => {
+export const meta: MetaFunction<typeof loader> = ({ loaderData }) => {
   return getMetaTagsForSite({
-    title: data?.config?.title,
-    description: data?.config?.description,
-    twitter: data?.config?.options?.twitter,
+    title: loaderData?.config?.title,
+    description: loaderData?.config?.description,
+    twitter: loaderData?.config?.options?.twitter,
   });
 };
 
@@ -49,13 +49,10 @@ export const links: LinksFunction = () => {
 };
 
 export const loader: LoaderFunction = async ({ request }): Promise<SiteLoader> => {
-  const [config, themeSession] = await Promise.all([
-    getConfig().catch(() => null),
-    getThemeSession(request),
-  ]);
+  const baseURL = process.env.BASE_URL || undefined;
+  const config =     getConfig().catch(() => null);
   if (!config) throw responseNoSite();
   const data = {
-    theme: themeSession.getTheme(),
     config,
     CONTENT_CDN_PORT: process.env.CONTENT_CDN_PORT ?? 3100,
     MODE: (process.env.MODE ?? 'app') as 'app' | 'static',
@@ -64,11 +61,10 @@ export const loader: LoaderFunction = async ({ request }): Promise<SiteLoader> =
   return data;
 };
 
-export default function AppWithReload() {
-  const { theme, config, CONTENT_CDN_PORT, MODE, BASE_URL } = useLoaderData<SiteLoader>();
+export default function App() {
+  const { config, CONTENT_CDN_PORT, MODE, BASE_URL } = useLoaderData<SiteLoader>();
   return (
     <Document
-      theme={theme}
       config={config}
       scripts={MODE === 'static' ? undefined : <ContentReload port={CONTENT_CDN_PORT} />}
       staticBuild={MODE === 'static'}

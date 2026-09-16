871894d4fbe289a5f1f4b6397bd249b87d5f359d
Angus Hollands <goosey15@gmail.com>
2026-09-16 15:03:25 +0100

    refactor: simplify config handling

---
+++ themes/book/vite.config.ts
@@ -0,0 +1,21 @@
+import { reactRouter } from '@react-router/dev/vite';
+import { envOnlyMacros } from 'vite-env-only';
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      port: 3000,
+    },
+    base: process.env.BASE_URL ?? '/',
+    build: {
+      minify: mode === 'production',
+    },
+    plugins: [reactRouter(), envOnlyMacros()],
+
+    resolve: { tsconfigPaths: true },
+    optimizeDeps: {
+      exclude: [],
+    },
+  };
+});

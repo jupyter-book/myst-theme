85b34dedc3caa4a223fb00794b256d4e86935693
Angus Hollands <goosey15@gmail.com>
2026-09-17 17:52:00 +0100

    fix: clean up error handling

--- packages/site/src/pages/ErrorUnhandled.tsx
+++ packages/site/src/pages/ErrorUnhandled.tsx
@@ -1,14 +1,14 @@
-export type ErrorResponse = {
-  status: number;
-  statusText: string;
-  data: any;
-};
-export function ErrorUnhandled({ error }: { error: ErrorResponse }) {
-  return (
-    <>
-      <h1>Unexpected Error Occurred</h1>
-      <p>Status: {error.status}</p>
-      <p>{error.data?.message ?? ''}</p>
-    </>
-  );
+export function ErrorUnhandled({ error }: { error: unknown }) {
+  if (error instanceof Error) {
+    return (
+      <>
+        <h1>Unexpected Error Occurred</h1>
+        <p>{error.message}</p>
+        <p>The stack trace is:</p>
+        <pre>{error.stack}</pre>
+      </>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
 }

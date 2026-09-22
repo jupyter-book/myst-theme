1f3db741d841b8d2a6ae9a169a51b179546180bf
Angus Hollands <goosey15@gmail.com>
2026-09-22 08:09:59 +0100

    chore: remove unused files

--- packages/prerender/src/config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from '@react-router/dev/config';
-
-import { buildEnd } from './buildEnd.js';
-import { prerender } from './prerender.js';
-
-export const config = {
-  prerender,
-  buildEnd,
-} satisfies Config;

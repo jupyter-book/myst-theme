4d286bab28f5918883610953e6b3927881e8da5c
Angus Hollands <goosey15@gmail.com>
2026-09-22 08:31:24 +0100

    fix: copy assets

--- themes/book/app/prerender.ts
+++ themes/book/app/prerender.ts
@@ -1,6 +1,14 @@
 import { prerender } from '@myst-theme/prerender';
+import fs from 'node:fs/promises';
 import * as build from 'virtual:react-router/server-build';
 
-const outPath = process.env.OUTDIR ? process.env.OUTDIR : './build/client';
+const ASSETS_PATH = './build/client';
+const OUT_PATH = process.env.BUILD_DIRECTORY;
+if (OUT_PATH === undefined) {
+  throw new Error('Missing build path');
+}
 
-await prerender(build, outPath);
+await fs.rm(OUT_PATH);
+await fs.cp(ASSETS_PATH, OUT_PATH, { recursive: true });
+
+await prerender(build, OUT_PATH);

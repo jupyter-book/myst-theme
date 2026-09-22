2559da61ca8997d178dc92066f8dcc222d1d0fc9
choldgraf <choldgraf@gmail.com>
2026-09-22 12:18:19 -0700

    Make links in the navbar look like UI not text links

--- packages/site/src/components/Navigation/TopNav.tsx
+++ packages/site/src/components/Navigation/TopNav.tsx
@@ -170,7 +170,7 @@ export function TopNav({
           <ThemeButton className="w-8 h-8 ml-3" />
           {/* Custom part at end of navbar. It is `hidden` up until xl size since it will be in the sidebar drawer up to that point */}
           {navbarEnd && (
-            <div className="article myst-navbar-end hidden xl:flex items-center ml-3 [&>*]:m-0">
+            <div className="article ui-links myst-navbar-end hidden xl:flex items-center ml-3 [&>*]:m-0">
               <MyST ast={navbarEnd} />
             </div>
           )}

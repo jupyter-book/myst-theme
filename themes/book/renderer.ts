85d5460de2ddeae32fd7793beedc2a71eaf94f16
Angus Hollands <goosey15@gmail.com>
2026-09-24 21:20:46 +0100

    fix: assets

--- themes/book/renderer.ts
+++ themes/book/renderer.ts
@@ -1,16 +1,15 @@
 import { prerender } from '@myst-theme/prerender';
 import fs from 'node:fs/promises';
+import path from 'node:path';
 import * as build from 'virtual:react-router/server-build';
 
-process.env.NODE_ENV = process.env.NODE_ENV ?? 'production';
-
-const ASSETS_PATH = './build/client';
+const CLIENT_PATH = path.join(path.dirname(import.meta.dirname), 'client');
 const OUT_PATH = process.env.BUILD_DIRECTORY;
 if (OUT_PATH === undefined) {
   throw new Error('Missing build path');
 }
 
 await fs.rm(OUT_PATH, { recursive: true, force: true });
-await fs.cp(ASSETS_PATH, OUT_PATH, { recursive: true });
+await fs.cp(CLIENT_PATH, OUT_PATH, { recursive: true });
 
 await prerender(build, OUT_PATH);
